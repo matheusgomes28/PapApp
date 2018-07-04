bb0c9629ed569e8d8ef505faefd7d23da5b7fb68
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2018-07-04 17:51:37 +0100

    Improved the detection of possible microaneurysms by using the 'blob detection' algorithm available in OpenCV. Will document this stuff o the weekend. Curently working on the CNN architecture.

--- frontend/vessel_segment.py
+++ frontend/vessel_segment.py
@@ -50,72 +50,6 @@ def masked_array_func(func, array, mask):
 
     return func(array[mask])
 
-def breadth_first(image_original, loc):
-    """
-    This function will run breadth first 
-    search on a mask, given a starting pixel.
-    The visited (white) pixels are turned to
-    black.
-
-    Arguments:
-        image_original - The np array representing the mask.
-        loc            - The initial location to start.
-
-    Returns:
-        Nump array with the searched image, white 
-        pixels represent the unvisited pixels.
-    """
-
-    def get_neighbours(image, loc):
-        """
-        Inner function that will get the neighbouring 
-        pixels of the given pixel in the image.
-        """
-
-    
-        # Get  the locations to look
-        left, right = loc[1]-1, loc[1]+1
-        up, down = loc[0]-1, loc[0]+1
-
-        # Placeholder for the location queue
-        locs = []
-
-        # Adding the locations
-        if left >= 0 and image[loc[0], left] > 0:
-            locs.append([loc[0], left])
-        if right <= image.shape[1] and image[loc[0], right] > 0:
-            locs.append([loc[0], right])
-        if up >= 0 and image[up, loc[1]] > 0:
-            locs.append([up, loc[1]])
-        if down <= image.shape[0] and image[down, loc[1]] > 0:
-            locs.append([down, loc[1]])
-
-        return locs
-
-    # Get a copy of the image
-    image = np.copy(image_original)
-
-    # Create a queue (each row is a position to visit)
-    queue = np.zeros((1,2))
-    queue[0,:] = loc # First index is the location passed
-
-    # Now just perform breadth first
-    while queue.shape[0] > 0:
-
-        # Visit the pixel in the image
-        current_loc = queue[0,:].astype(int)
-
-        if image[tuple(current_loc)] > 0:
-
-            # Get the neighbours and add to the queue
-            ns = get_neighbours(image, current_loc)
-            if len(ns) > 0: queue = np.vstack((queue, ns))
-
-        queue = np.delete(queue, (0), axis=0)
-        image[tuple(current_loc)] = 0
-
-    return image
-
 def main():
     """
     Main script here. This is where all the 
@@ -125,6 +59,7 @@ def main():
     ###############################
     ## Parsing the cmd line args ##
     ###############################
+
     args = docopt(__doc__)
     path = files.abspath(args["PATH"])
 
@@ -135,6 +70,11 @@ def main():
 
     # Open the image
     image = ut.read_image(path)[:,:,1]
+    
+
+    #######################
+    ## Vessel Extraction ##
+    #######################
 
     # Dilate the image and erode it
     kernel = np.ones((5,5))
@@ -145,12 +85,13 @@ def main():
     diff = (eroded - image)
     
     # Apply the circular mask to the fundus
-    mask = ft.circular_mask(diff.shape, 400, 450)
+    mask = ft.circular_mask(diff.shape, 600, 601)
     diff = (diff*mask).astype(np.uint8)
     diff = np.dstack((diff, diff, diff))
 
     # Apply the mean shift to smoothen the noise
     diff_shifted = cv2.pyrMeanShiftFiltering(diff, 10, 15)
+    diff_shifted = diff_shifted[:,:,0].astype(np.uint8)
 
     # Now calculate the mask with basic mean comparissons
     colour_mean = masked_array_func(np.mean, diff_shifted, mask>0)
@@ -158,11 +99,42 @@ def main():
     print(colour_mean, colour_var)
 
     # Get the vessel network mask
-    vessel_mask = (diff_shifted[:,:,0] >= colour_mean+np.sqrt(colour_var))*255
-
-    # Do the segmenting on the image
-    bad_areas = breadth_first(vessel_mask, (493,996))
+    thresh_type = cv2.THRESH_BINARY_INV
+    thresh_val = colour_mean + np.sqrt(colour_var)
+    deduct = 5; size = 9 # Mean deduction and block size
+    ret_val, vessel_mask = cv2.threshold(diff_shifted, thresh_val,
+                           255, thresh_type)
     
+
+    
+    ##########################
+    ## Blob Detection Stuff ##
+    ##########################
+
+    # Create the parameters and filtering options
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.025
+    params.blobColor = 0
+
+    # Creat the detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # Get the keypoints with the detector
+    keypoints = detector.detect(vessel_mask)
+    kpt = keypoints[2]
+
+    print("Point: ", kpt.pt)
+    print("Octave: ", kpt.octave)
+    print("Size: ", kpt.size)
+    print("Angle: ", kpt.angle)
+    print("Overlap: ", kpt.overlap)
+
+    # Draw the blobs detected
+    flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS # To match size
+    out_img = np.zeros(vessel_mask.shape).astype(np.uint8) # Empty (but it is required)
+    blob_img = cv2.drawKeypoints(out_img, keypoints, np.array([]), 255, flags)
+
     # Display the image (for testing only)
     fig = plt.figure(figsize=(17,5))
 
@@ -174,12 +146,12 @@ def main():
     ax2 = plt.subplot(1,3,2) 
     ax2.set_axis_off()
     ax2.set_title("Vessel Network Mask")
-    ax2.imshow(diff_shifted, cmap="gray")
+    ax2.imshow(vessel_mask, cmap="gray")
 
     ax3 = plt.subplot(1,3,3)
     ax2.set_axis_off()
     ax3.set_title("Detected Outside Network")
-    ax3.imshow(bad_areas, cmap="gray")
+    ax3.imshow(blob_img, cmap="gray")
     fig.tight_layout(); plt.show();
 
 if __name__ == "__main__":
